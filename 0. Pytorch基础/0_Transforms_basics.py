a6ddcb389e786b7673f8eb8fdb98021ab80b8c16
boris sigma <1322553126@qq.com>
2024-09-01 17:14:57 +0800

    Update 0_Transforms_basics.py

--- 0. Pytorch基础/0_Transforms_basics.py
+++ 0. Pytorch基础/0_Transforms_basics.py
@@ -6,8 +6,16 @@ Created on 2024-09-01 15:04:09
 """
 
 from torchvision import transforms
+from PIL import Image
+
 
 if __name__ == '__main__':
-    # 1. transforms如何使用
-    # 2. 为什么需要Tensor数据类型
-    a = 1
+    image_path = r'C:\Users\borisσ\Desktop\boris_Inc\_Github\wode_LLMs_practice\hymenoptera_data\train\ants\0013035.jpg'
+    image = Image.open(image_path)
+
+    # 1. transforms如何使用?
+    transform = transforms.Compose([transforms.ToTensor()])
+    image_transformed = transform(image)
+    print("The size of original image: ", image.size, "The shape of transformed image: ", image_transformed.shape)
+
+    # 2.
