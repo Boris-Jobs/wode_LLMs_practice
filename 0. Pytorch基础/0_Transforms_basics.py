61475f62b77a1af8d0b7efd4c309b6d029bb7c52
boris sigma <1322553126@qq.com>
2024-09-01 17:48:56 +0800

    transforms study

--- 0. Pytorch基础/0_Transforms_basics.py
+++ 0. Pytorch基础/0_Transforms_basics.py
@@ -7,6 +7,7 @@ Created on 2024-09-01 15:04:09
 
 from torchvision import transforms
 from PIL import Image
+from torch.utils.tensorboard import SummaryWriter
 
 
 if __name__ == '__main__':
@@ -14,8 +15,14 @@ if __name__ == '__main__':
     image = Image.open(image_path)
 
     # 1. transforms如何使用?
-    transform = transforms.Compose([transforms.ToTensor()])
+    trans_totensor = transforms.ToTensor()
+    trans_normalize = transforms.Normalize([6, 3, 2], [9, 3, 5])
+    transform = transforms.Compose([trans_totensor, trans_normalize])
     image_transformed = transform(image)
     print("The size of original image: ", image.size, "The shape of transformed image: ", image_transformed.shape)
-
-    # 2.
+    # 写入tensorboard中:
+    writer = SummaryWriter(log_dir='logs')
+    writer.add_image('image_transformed', image_transformed)
+    writer.close()
+    print(image_transformed[2][0][0])
+    # cmd: tensorboard --logdir=logs
