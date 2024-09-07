cc4499b7e099b2b2955bcf2069d8faa2a47237da
boris sigma <1322553126@qq.com>
2024-09-08 00:24:23 +0800

    Update 0_Scaled_Dot_Product_Attention.py

--- 2_transformer_basics/0_Scaled_Dot_Product_Attention.py
+++ 2_transformer_basics/0_Scaled_Dot_Product_Attention.py
@@ -39,12 +39,44 @@ print(weights.sum(dim=-1))
 atten_outputs = torch.bmm(weights, V)
 
 
-# 7. 封装成函数
+# 7. 封装scaled dot product attention函数
 def scaled_dot_product_attention(
     query, key, value, query_mask=None, key_mask=None, mask=None
 ):
     dim_k = query.shape[-1]
     scores = torch.bmm(query, key.transpose(1, 2))
+    # [batch_size, query_length, key_length]
     if query_mask is not None and key_mask is not None:
         mask = torch.bmm(query_mask.unsqueeze(-1), key_mask.unsqueeze(1))
-    
+        # query_mask.unsqueeze(-1) -- [batch_size, query_length, 1]
+        # key_mask.unsqueeze(1) -- [batch_size, 1, key_length]
+        # mask
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, -float('inf'))
+
+    weights = F.softmax(scores, dim=-1)
+    return torch.bmm(weights, value)
+
+# 8. 注意力头类
+class AttentionHead(nn.Module):
+    def __init__(self, embed_dim, head_dim):
+        super().init()
+        self.q = nn.Linear(embed_dim, head_dim)
+        self.k = nn.Linear(embed_dim, head_dim)
+        self.v = nn.Linear(embed_dim, head_dim)
+
+    def forward(self, query, key, value, query_mask=None, key_mask=None, mask=None):
+        attn_outputs = scaled_dot_product_attention(self.q(query), self.k(key), self.v(value), query_mask, key_mask, mask)
+        return atten_outputs
+
+# 9. 多头Attention类
+class MultiHeadAttention(nn.Module):
+    def __init__(self, config):
+
+        
+        super().__init__()
+        embed_dim = config.hidden_size
+        num_heads = config.num_attention_heads
+        head_dim = embed_dim // num_heads
+        self.heads = nn.ModuleList([AttentionHead(embed_dim, head_dim) for _ in range(num_heads)])
+        self.output_linear = nn.Linear(embed_dim, embed_dim)
